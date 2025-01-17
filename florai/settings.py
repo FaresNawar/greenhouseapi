1b4345bf4b284bee2d415a1415a8e1b3b28e0fe8
FaresNawar <faresmostafanawar@gmail.com>
2025-01-17 23:44:39 +0200

    sd

--- florai/settings.py
+++ florai/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'florai.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgersql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',    
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD': 'vkTovtkysONhJIxZVnvOGPUDAcvaohqp',
+        'HOST':'autorack.proxy.rlwy.net',
+        'PORT':'54994',
     }
 }
 
