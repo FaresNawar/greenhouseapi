0f9ad4c7a81a98f7235e4b22de377156f1680a19
FaresNawar <faresmostafanawar@gmail.com>
2025-01-18 01:27:59 +0200

    abdelaziz can suck it

--- florai/settings.py
+++ florai/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'project',
     'rest_framework',
     'rest_framework.authtoken',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'florai.urls'
